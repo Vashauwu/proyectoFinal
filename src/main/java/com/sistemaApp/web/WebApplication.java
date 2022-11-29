b0b66deb616e30e61f47e9b2ba50510d7caf09f5
pc <adrian.valladarez@gmail.com>
2022-11-28 21:27:28 -0600

    proyecto con SSL

--- src/main/java/com/sistemaApp/web/WebApplication.java
+++ src/main/java/com/sistemaApp/web/WebApplication.java
@@ -2,29 +2,71 @@ package com.sistemaApp.web;
 
 import com.sistemaApp.web.entidad.Student;
 import com.sistemaApp.web.repository.StudentRepository;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
+//@EnableWebSecurity
 @SpringBootApplication
-public class WebApplication implements CommandLineRunner{
+public class WebApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-            
-		SpringApplication.run(WebApplication.class, args);
-	}
+    public static void main(String[] args) {
 
-        
-     @Autowired   
-     private StudentRepository repo;
-     
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+    @Autowired
+    private StudentRepository repo;
+
     @Override
     public void run(String... args) throws Exception {
 //      Student estudiante = new Student("Alberto","Valladares","10037295","adrian.valladarez@gmail.com");
 //      repo.save(estudiante);
 //      Student estudiante2 = new Student("Adrian","Valladares","10037295","adrian.valladares@gmail.com");
 //      repo.save(estudiante2);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        // Enable SSL Trafic
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        // Add HTTP to HTTPS redirect
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+        return tomcat;
+    }
+
+    /*
+    We need to redirect from HTTP to HTTPS. Without SSL, this application used
+    port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
+    redirected to HTTPS on 8443.
+     */
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8081);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
     }
 
 }
---
+++ src/main/java/com/sistemaApp/web/controller/LoginController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sistemaApp.web.controller;
+
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+///**
+
+//public class LoginController {
+//    
+//     @GetMapping("/admin")
+//     @ResponseBody
+//     @PreAuthorize("hasAuthority('APPROLE_Admin')")
+//     public String Admin() {
+//         return "Admin message";
+//     }
+//
+//}
